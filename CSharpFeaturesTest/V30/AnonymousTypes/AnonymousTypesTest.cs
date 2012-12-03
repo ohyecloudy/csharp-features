df1ad40bb491be0b459d76b549885723c8311123
ohyecloudy <ohyecloudy@gmail.com>
2012-12-03 22:08:17 +0900

    v30 features
    * anonymous types

---
+++ CSharpFeaturesTest/V30/AnonymousTypes/AnonymousTypesTest.cs
@@ -0,0 +1,24 @@
+﻿// 참고:
+// Anonymous Types (C# Programming Guide) - msdn
+// http://msdn.microsoft.com/en-us/library/bb397696
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpFeaturesTest.V30.AnonymousTypes
+{
+    [TestClass]
+    public class AnonymousTypesTest
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var v = new { Amount = 108, Message = "Hello" };
+            
+            // read only!
+            // v.Amount = 110;
+
+            Assert.AreEqual("<>f__AnonymousType0`2", v.GetType().Name, "2는 패러매터 숫자를 가리킨다");
+            Assert.AreEqual("System.Object", v.GetType().BaseType.FullName, "object 상속");
+        }
+    }
+}
